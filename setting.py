9b594e72610671755e35322fbbaa2e2183dd9e45
sgiallourakis <sgiallourakis3@gmail.com>
2023-04-07 13:32:25 -0400

    Initial Upload

---
+++ setting.py
@@ -0,0 +1,17 @@
+level_map = [
+'                    ',
+'                    ',
+'                    ',
+'XX   XXX     XX     ',
+'XX  XX              ',
+'XXX       XX      XX',
+'XXXX     XXX        ',
+'XX    X  XXX    XX  ',
+'    XXX  XXX   XXXX ',
+'   XXXX  XXX   XXXXX',
+'XXXXXXX  XXX   XXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+print(screen_height)
