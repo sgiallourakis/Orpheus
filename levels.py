b934a406b5317171521d07a11f0164101e016da7
sgial <Athenselya11!>
2023-04-09 19:25:03 -0400

    get tile system working, needed to change Level.run() to level.run() to remove self error

--- levels.py
+++ levels.py
@@ -2,20 +2,25 @@ import pygame
 from tiles import Tile
 from setting import tile_size
 
+
 class Level:
-    def __init__(self,level_data,surface):
+    def __init__(self, level_data, surface):
+        self.tiles = pygame.sprite.Group()
         self.display_surface = surface
         self.setup_level(level_data)
 
-   """def setup_level(self,layout):
-        self.tiles = pygame.sprite.Group()
-        for row_index,row in enumerate(layout):
-            for col_index,cell in enumerate(row):
-               if cell == 'X':
-                   x = col_index
-                   y = row_index * tile_size
-                   tile = Tile((x,y),tile_size)
-                   self.tiles.add(tile)
-"""
-    def ren(self):
+    def setup_level(self, layout):
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                if cell =='X':
+                    x = row_index
+                    y = col_index
+                    tile = Tile((x, y), tile_size)
+                    # noinspection PyTypeChecker
+                    self.tiles.add(tile)
+
+
+    def run(self):
         self.tiles.draw(self.display_surface)
+
+
--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import pygame
 import sys
-#from levels import Level
+from levels import Level
 from setting import *
-from tiles import Tile
 
-#Pygame, setup
+# Pygame, setup
 
 pygame.init()
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-#level = Level(level_map,screen)
-test_tile = pygame.sprite.Group(Tile((100,100),200))
-
+level = Level(level_map, screen)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	screen.fill('black')
-	test_tile.draw(screen)
+    screen.fill('black')
+    level.run()
 
-	pygame.display.update()
-	clock.tick(60)
+    pygame.display.update()
+    clock.tick(60)
