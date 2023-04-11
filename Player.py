e6098e4ae0dd27d85fea8eb41c61f60ca8ac61fe
sgiallourakis <sgiallourakis3@gmail.com>
2023-04-11 10:42:44 -0400

    Update player can now move in the x direction, screen can kind of scroll with player now

--- Player.py
+++ Player.py
@@ -1,5 +1,4 @@
 import pygame
-from setting import tile_size
 
 
 class Player(pygame.sprite.Sprite):
@@ -8,3 +7,18 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 32))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.Vector2(0, 0)
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * 5
